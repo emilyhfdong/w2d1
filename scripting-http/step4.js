28719ed49ad9f442bd7bd90c49bd4f50e843f826
Emily Dong <emilyhfdong@gmail.com>
2018-06-11 15:33:22 +0000

    step 4

---
+++ scripting-http/step4.js
@@ -0,0 +1,33 @@
+var https = require('https');
+
+function getAndPrintHTML (options, callback) {
+
+  https.get(options, function (response) {
+    var allData = '';
+
+
+    response.setEncoding('utf8');
+
+    response.on('data', function(data) {
+      allData += data;
+    });
+
+    response.on('end', function(end) {
+      callback(allData);
+    });
+
+
+  })
+
+}
+
+function printHTML (html) {
+  console.log(html);
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+
+getAndPrintHTML(requestOptions, printHTML);
